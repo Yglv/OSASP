9fa891954cdd2ed682376fbd2031a1b6383f6b86
Yglv <78694320+Yglv@users.noreply.github.com>
2022-04-11 09:05:06 +0300

    Update task-3.c

--- Lab_2/task-3.c
+++ Lab_2/task-3.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <limits.h>
 
 int main(int argc,char* argv[])
 {
     if (argc!=3)
     {
-        fprintf(stderr,"Unexpected number of parameters\n");
+        fprintf(stderr,"Unexpected number of parameters\n.Command format:%s filename line_num\n",argv[0]);
+        fprintf(stderr,"filename - name of file to read\n");
+        fprintf(stderr,"line_num - count of lines\n");
         return 1;
     }
     char* filename=argv[1];
@@ -15,10 +19,22 @@ int main(int argc,char* argv[])
         perror("fopen()");
         return 1;
     }
-    int line_num=strtol(argv[2],NULL,10);
+    errno=0;
+    char *endptr;
+    long line_num=strtol(argv[2],&endptr,10);
+    if ((errno == ERANGE && (line_num == LONG_MAX || line_num == LONG_MIN)) || (errno != 0 && line_num == 0)) 
+    {
+        perror("strtol()");
+        exit(EXIT_FAILURE);
+    }
+    if (endptr == argv[2]) 
+    {
+        fprintf(stderr, "No digits were found\n");
+        exit(EXIT_FAILURE);
+    }
     if (line_num<0)
     {
-        fprintf(stderr,"The number of lines must be >=0");
+        fprintf(stderr,"Invalid count of lines\n");
         fclose(f);
         return 1;
     }
